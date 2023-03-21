e4774a9da090226ead15fa9dc896b03cc28a52e1
Stasikon <lecsa515@gmail.com>
2023-03-21 16:45:52 +0300

    Int commit Final Object Methods

--- src/Book.java
+++ src/Book.java
@@ -1,21 +1,40 @@
+import java.util.Objects;
+
 public class Book {
     private String bookName;
     private Author author;
     private int publishYear;
-
     public Book(String bookName, Author author, int publishYear) {
-            this.bookName = bookName;
-            this.author = author;
-            this.publishYear = publishYear;
-        }
-        public String getBookName() {
-            return bookName;
-        }
-        public int getPublishYear() {
-            return publishYear;
-        }
-        public void setPublishYear(int publishYear) {
-            this.publishYear = publishYear;
-        }
+        this.bookName = bookName;
+        this.author = author;
+        this.publishYear = publishYear;
     }
+    public String getBookName() {
+        return bookName;
+    }
+    public int getPublishYear() {
+        return publishYear;
+    }
+    public void setPublishYear(int publishYear) {
+        this.publishYear = publishYear;
+    }
+    @Override
+    public String toString() {
+        return author + ", книга: " + bookName + " , год публикации: " + publishYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publishYear == book.publishYear && Objects.equals(bookName, book.bookName) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, author, publishYear);
+    }
+}
+
 
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 public class Main {
-    public static void main(String[] args) {
-        Author keyes = new Author("Дэниел", "Киз");
-        Book multipleMindsofBilly = new Book("Множественные умы Билли Миллигана", keyes, 1970);
-        System.out.println(multipleMindsofBilly.getBookName() + ", " + keyes.getName() + " " + keyes.getSurname() + ", " + multipleMindsofBilly.getPublishYear());
-        Author melnikova = new Author("Мария ", "Мельникова");
-        Book psychologyFingers = new Book("Психология на пальцах", melnikova, 2019);
-        System.out.println(psychologyFingers.getBookName() + ", " + melnikova.getName() + " " + melnikova.getSurname() + ", " + psychologyFingers.getPublishYear());
-        psychologyFingers.setPublishYear(2019);
-        System.out.println(psychologyFingers.getBookName() + ", " + melnikova.getName() + " " + melnikova.getSurname() + ", " + psychologyFingers.getPublishYear());
-        Author linehan = new Author("Марша", "Линехан");
-        Book borderlinePersonalityDisorder = new Book("Когнитивно-поведенческая терапия пограничного расстройства личности", linehan, 1983);
-        System.out.println(borderlinePersonalityDisorder.getBookName() + ", " + linehan.getName() + " " + linehan.getSurname() + ", " + borderlinePersonalityDisorder.getPublishYear());
-    }
+   public static void main(String[] args) {
+       Author keyes = new Author("Дэниел", "Киз");
+       Book multipleMindsofBilly = new Book("Множественные умы Билли Миллигана", keyes, 1970);
+       System.out.println(multipleMindsofBilly);
+       Author melnikova = new Author("Мария ", "Мельникова");
+       Book psychologyFingers = new Book("Психология на пальцах", melnikova, 2019);
+       System.out.println(psychologyFingers);
+       psychologyFingers.setPublishYear(2019);
+       System.out.println(psychologyFingers);
+       Author linehan = new Author("Марша", "Линехан");
+       Book borderlinePersonalityDisorder = new Book("Когнитивно-поведенческая терапия пограничного расстройства личности", linehan, 1993);
+       System.out.println(borderlinePersonalityDisorder);
+       Book theBlackCollar1 = new Book("Когнитивно-поведенческая терапия пограничного расстройства личности", linehan, 1993);
+       System.out.println(theBlackCollar1.equals(borderlinePersonalityDisorder));
+       System.out.println(theBlackCollar1.equals(psychologyFingers));
+       System.out.println(borderlinePersonalityDisorder.hashCode());
+   }
 }
